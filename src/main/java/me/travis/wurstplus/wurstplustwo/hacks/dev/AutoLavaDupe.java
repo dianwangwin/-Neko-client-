c4258911b7920a736ea8af74b1a687f2473cafce
rTe_ <69632153+rTeeee@users.noreply.github.com>
2021-04-04 23:17:47 +0100

    auto lava dupe

---
+++ src/main/java/me/travis/wurstplus/wurstplustwo/hacks/dev/AutoLavaDupe.java
@@ -0,0 +1,15 @@
+package me.travis.wurstplus.wurstplustwo.hacks.dev;
+
+import me.travis.wurstplus.wurstplustwo.hacks.WurstplusCategory;
+import me.travis.wurstplus.wurstplustwo.hacks.WurstplusHack;
+
+public class AutoLavaDupe extends WurstplusHack {
+
+    public AutoLavaDupe() {
+        super(WurstplusCategory.WURSTPLUS_BETA);
+
+        this.name        = "Auto Lava Dupe";
+        this.tag         = "AutoLavaDupe";
+        this.description = "lava dupe!!!!";
+   }
+}
