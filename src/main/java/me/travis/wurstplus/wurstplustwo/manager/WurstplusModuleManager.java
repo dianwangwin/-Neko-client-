ee10341ddb73c13c0127cdffb206e4897f2e3410
Crybaby6 <75589614+Crybaby6@users.noreply.github.com>
2021-04-04 22:29:41 +0300

    fixed da build fail
    ease

--- src/main/java/me/travis/wurstplus/wurstplustwo/manager/WurstplusModuleManager.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/manager/WurstplusModuleManager.java
@@ -79,7 +79,7 @@ public class WurstplusModuleManager {
 		add_hack(new WurstplusStrafe());
 		add_hack(new WurstplusStep());
 		add_hack(new WurstplusSprint());
-		add_hack(new NoPush());
+		
 
 		// Render.
 		add_hack(new WurstplusHighlight());
