b87cad8f4f8380308997d2ac18a2dfb58372422d
rTe_ <69632153+rTeeee@users.noreply.github.com>
2021-04-05 01:01:35 +0100

    Update AutoLavaDupe.java

--- src/main/java/me/travis/wurstplus/wurstplustwo/hacks/dev/AutoLavaDupe.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/hacks/dev/AutoLavaDupe.java
@@ -11,15 +11,12 @@ public class AutoLavaDupe extends WurstplusHack {
         this.name        = "Auto Lava Dupe";
         this.tag         = "AutoLavaDupe";
         this.description = "lava dupe!!!!";
-       }
+       
+    
+    }
 
     
-       public void enable() {
-         mc.field_71439_g.func_71165_d("i am a retard and fell for nekohax's jewish trickery!");
-         mc.field_71439_g.func_71165_d("/kill");
-         this.set_disable();
-       }
-      }
+
 
 
 
