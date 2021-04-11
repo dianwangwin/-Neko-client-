8849559ca86cbcbfe2b6b69e0951b0056a9eff9c
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-04-10 18:25:44 -0700

    Update InventoryMove.java

--- src/main/java/me/travis/wurstplus/wurstplustwo/hacks/movement/InventoryMove.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/hacks/movement/InventoryMove.java
@@ -1,80 +1,60 @@
 package me.travis.wurstplus.wurstplustwo.hacks.movement;
 
+import java.util.function.Predicate;
 import me.travis.wurstplus.wurstplustwo.event.events.WurstplusEventGUIScreen;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusCategory;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusHack;
 import me.zero.alpine.fork.listener.EventHandler;
 import me.zero.alpine.fork.listener.Listener;
-
-import net.minecraftforge.client.settings.IKeyConflictContext;
-import net.minecraftforge.client.settings.KeyConflictContext;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.client.gui.GuiChat;
-
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.settings.KeyConflictContext;
 import org.lwjgl.input.Keyboard;
-public class InventoryMove extends WurstplusHack {
+
+public class InventoryMove
+extends WurstplusHack {
+    private static final KeyBinding[] KEYS = new KeyBinding[]{InventoryMove.mc.gameSettings.keyBindForward, InventoryMove.mc.gameSettings.keyBindRight, InventoryMove.mc.gameSettings.keyBindBack, InventoryMove.mc.gameSettings.keyBindLeft, InventoryMove.mc.gameSettings.keyBindJump, InventoryMove.mc.gameSettings.keyBindSprint};
+    @EventHandler
+    private final Listener<WurstplusEventGUIScreen> state_gui;
+
     public InventoryMove() {
         super(WurstplusCategory.WURSTPLUS_MOVEMENT);
         this.name = "Inventory Move";
         this.tag = "InventoryMove";
         this.description = "move in guis";
-
+        this.state_gui = new Listener<WurstplusEventGUIScreen>(wurstplusEventGUIScreen -> {
+            if (InventoryMove.mc.player == null && InventoryMove.mc.world == null) {
+                return;
+            }
+            if (wurstplusEventGUIScreen.get_guiscreen() instanceof GuiChat || wurstplusEventGUIScreen.get_guiscreen() == null) {
+                return;
+            }
+            this.walk();
+        }, new Predicate[0]);
     }
-
-    private static final KeyBinding[] KEYS = new KeyBinding[]{mc.gameSettings.keyBindForward, mc.gameSettings.keyBindRight, mc.gameSettings.keyBindBack, mc.gameSettings.keyBindLeft, mc.gameSettings.keyBindJump, mc.gameSettings.keyBindSprint};
-    int JUMP                         = mc.gameSettings.keyBindJump.getKeyCode();
-
-    @EventHandler
-    public Listener<WurstplusEventGUIScreen.Displayed> listener = new Listener<>(event -> {
-        if (mc.currentScreen instanceof GuiChat || mc.currentScreen == null) {
-            return;
-        }
-
-        walk();
-    });
 
     @Override
     public void update() {
-        if (mc.currentScreen instanceof GuiChat || mc.currentScreen == null) {
+        if (InventoryMove.mc.player == null && InventoryMove.mc.world == null) {
             return;
         }
-
-        mc.player.rotationYaw  += Keyboard.isKeyDown(205) ? 4.0f : (Keyboard.isKeyDown(203) ? -4.0f : 0.0f);
-        mc.player.rotationPitch = (float) (mc.player.rotationPitch + (Keyboard.isKeyDown(208) ? 4 : (Keyboard.isKeyDown(200) ? -4 : 0)) * 0.75);
-        mc.player.rotationPitch = MathHelper.clamp(mc.player.rotationPitch, - 90.0f, 90.0f);
-
-        if (Keyboard.isKeyDown(JUMP)) {
-            if (mc.player.isInLava() || mc.player.isInWater()) {
-                mc.player.motionY += 0.38f;
-            } else {
-                if (mc.player.onGround) {
-                    mc.player.jump();
-                }
-            }
+        if (InventoryMove.mc.currentScreen instanceof GuiChat || InventoryMove.mc.currentScreen == null) {
+            return;
         }
-
-        walk();
+        this.walk();
     }
 
     public void walk() {
-        KeyBinding[] keys = KEYS;
-
-        int keys_n   = keys.length;
-        int keys_n_2 = 0;
-
-        while (keys_n_2 < keys_n) {
-            KeyBinding key_binding = keys[keys_n_2];
-
-            if (Keyboard.isKeyDown(key_binding.getKeyCode())) {
-                if (key_binding.getKeyConflictContext() != KeyConflictContext.UNIVERSAL) {
-                    key_binding.setKeyConflictContext(KeyConflictContext.UNIVERSAL);
+        for (KeyBinding keyBinding : KEYS) {
+            if (Keyboard.isKeyDown(keyBinding.getKeyCode())) {
+                if (keyBinding.getKeyConflictContext() != KeyConflictContext.UNIVERSAL) {
+                    keyBinding.setKeyConflictContext(KeyConflictContext.UNIVERSAL);
                 }
-
-                KeyBinding.setKeyBindState(key_binding.getKeyCode(), true);
-            } else {
-                KeyBinding.setKeyBindState(key_binding.getKeyCode(), false);
+                KeyBinding.setKeyBindState(keyBinding.getKeyCode(), true);
+                continue;
             }
-
-            ++keys_n_2;
+            KeyBinding.setKeyBindState(keyBinding.getKeyCode(), false);
         }
+    }
+}
+ 
