50a35aa868e01154bb8af247db4debd3bf89478b
rTe_ <69632153+rTeeee@users.noreply.github.com>
2021-04-13 09:58:06 +0100

    NoHandshake dumpstered on the oldest anarchy server in minecraft

--- src/main/java/me/travis/wurstplus/wurstplustwo/manager/WurstplusModuleManager.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/manager/WurstplusModuleManager.java
@@ -73,7 +73,6 @@ public class WurstplusModuleManager {
 		add_hack(new WurstplusEntityMine());
 		add_hack(new WurstplusBuildHeight());
 		add_hack(new WurstplusCoordExploit());
-		add_hack(new WurstplusNoHandshake());
 		add_hack(new Burrow());
 
 		// Movement.
