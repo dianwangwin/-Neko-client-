dbc2bcbfc94ae1be7031d6b9f982a1a60ffb291a
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-04-13 02:41:45 -0700

    Update WurstplusModuleManager.java

--- src/main/java/me/travis/wurstplus/wurstplustwo/manager/WurstplusModuleManager.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/manager/WurstplusModuleManager.java
@@ -80,6 +80,7 @@ public class WurstplusModuleManager {
 		add_hack(new WurstplusStrafe());
 		add_hack(new WurstplusStep());
 		add_hack(new WurstplusSprint());
+		add_hack(new WurstplusAnchor());
 		add_hack(new NoPush());
 		add_hack(new NoSlow());
 		add_hack(new InventoryMove());
