dbdcd8a7ae1e51dab4dec41346415660e1e3e1ba
ProfKambing <80607981+ProfKambing@users.noreply.github.com>
2021-04-16 23:43:15 +0800

    Update Watermark.java

--- src/main/java/lgbt/vaimok/neko/nekohax/guiscreen/hud/Watermark.java
+++ src/main/java/lgbt/vaimok/neko/nekohax/guiscreen/hud/Watermark.java
@@ -3,7 +3,7 @@ package lgbt.vaimok.neko.nekohax.guiscreen.hud;
 
 import lgbt.vaimok.neko.nekohax.NekoHax;
 import lgbt.vaimok.neko.nekohax.guiscreen.render.pinnables.Pinnable;
-import com.mojang.realmsclient.gui.ChatFormatting;
+
 
 
 public class Watermark extends Pinnable {
@@ -18,7 +18,7 @@ public class Watermark extends Pinnable {
 		int nl_b = NekoHax.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorB").get_value(255);
 		int nl_a = NekoHax.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorA").get_value(220);
 
-		String line = ChatFormatting.LIGHT_PURPLE + "NekoHax" + ChatFormatting.RESET + NekoHax.g + " v" + NekoHax.get_version();
+		String line = "NekoHax" + NekoHax.g + " v" + NekoHax.get_version();
 
 		create_line(line, this.docking(1, line), 2, nl_r, nl_g, nl_b, nl_a);
 
