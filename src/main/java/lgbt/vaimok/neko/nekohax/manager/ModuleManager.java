42f4d7e42ca37db1c356318756a51b340831aa0e
Crybaby6 <fallaway37@gmail.com>
2021-04-27 15:12:50 +0300

    mmmmm

--- src/main/java/lgbt/vaimok/neko/nekohax/manager/ModuleManager.java
+++ src/main/java/lgbt/vaimok/neko/nekohax/manager/ModuleManager.java
@@ -116,7 +116,7 @@ public class ModuleManager {
 		add_hack(new FastUtil());
 		add_hack(new Speedmine());
 		add_hack(new RichPresence());
-                add_hack(new MiddleClickXp());
+                add_hack(new MiddleClickXP());
 		
 
 		// Dev
