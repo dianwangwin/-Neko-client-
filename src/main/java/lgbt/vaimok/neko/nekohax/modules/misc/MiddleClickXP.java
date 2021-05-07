b2b323b7feab4759c08763fa8d1e536fab4f9a4c
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-05-06 23:31:57 -0700

    Update MiddleClickXP.java

--- src/main/java/lgbt/vaimok/neko/nekohax/modules/misc/MiddleClickXP.java
+++ src/main/java/lgbt/vaimok/neko/nekohax/modules/misc/MiddleClickXP.java
@@ -1,20 +1,18 @@
 package lgbt.vaimok.neko.nekohax.modules.misc;
 
-import lgbt.vaimok.neko.nekohax.Modules.Category;
-import lgbt.vaimok.neko.nekohax.Modules.Module;
+import lgbt.vaimok.neko.nekohax.modules.Category;
+import lgbt.vaimok.neko.nekohax.modules.Module;
 import lgbt.vaimok.neko.nekohax.util.InventoryUtil;
 import net.minecraft.init.Items;
 import net.minecraft.util.EnumHand;
 import org.lwjgl.input.Mouse;
 
 public class MiddleClickXP extends Module {
-    
     public MiddleClickXP() {
         super(Category.misc);
-
-		this.name        = "MiddleClickXP";
-		this.tag         = "MiddleClickXP";
-		this.description = "xp go brrrrrr";
+        this.name = "Middle Click XP";
+        this.tag = "MiddleClickXP";
+        this.description = "xp go brrrrrr";
     }
 
     public void update() {
